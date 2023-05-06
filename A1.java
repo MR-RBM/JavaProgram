e5d3d2bb3e40327e4a44ffb6f9d3c05e999d4a56
MR-RBM <120109634+MR-RBM@users.noreply.github.com>
2023-05-06 15:06:18 +0530

    Array sample

---
+++ A1.java
@@ -0,0 +1,21 @@
+package array;
+
+public class A1 {
+ public static void main(String[]arge) {
+	 int[] a=new int[5];
+	 System.out.println(a.length);//5
+	 
+	 a[0]=15;
+	 a[1]=30;
+	 a[2]=40;
+	 a[3]=50;
+	 a[4]=60;
+	 
+	 System.out.println(a[2]);
+	 System.out.println(a[4]);
+	 System.out.println(a[0]);
+	 System.out.println(a[5]);//AIOOBE
+	 
+ }
+}
+
